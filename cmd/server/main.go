e275d2ef10538f9ff97f321680894f184d291da4
Zino Kader <zinokad@gmail.com>
2021-10-03 14:43:46 +0200

    tidy up before branching out

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,15 @@
 package main
 
-import "www.github.com/ZinoKader/portal/pkg/server"
+import (
+	"math/rand"
+	"time"
+
+	"www.github.com/ZinoKader/portal/pkg/server"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
 
 func main() {
 	server.Start()
--- pkg/server/server.go
+++ pkg/server/server.go
@@ -2,38 +2,34 @@ package server
 
 import (
 	"log"
-	"math/rand"
 	"net/http"
 	"sync"
 	"time"
 )
 
 type Server struct {
-	router    *http.ServeMux
-	mailboxes *Mailboxes
+	httpServer *http.Server
+	router     *http.ServeMux
+	mailboxes  *Mailboxes
 }
 
 var server Server
-var httpServer *http.Server
 
 func init() {
-	rand.Seed(time.Now().UnixNano())
-	
 	server = Server{
+		httpServer: &http.Server{
+			Addr:         ":6969",
+			ReadTimeout:  30 * time.Second,
+			WriteTimeout: 30 * time.Second,
+			Handler:      server.router,
+		},
 		router:    http.NewServeMux(),
 		mailboxes: &Mailboxes{&sync.Map{}},
-	}
-
-	httpServer = &http.Server{
-		Addr:         ":6969",
-		ReadTimeout:  30 * time.Second,
-		WriteTimeout: 30 * time.Second,
-		Handler:      server.router,
 	}
 
 	server.routes()
 }
 
 func Start() {
-	log.Fatal(httpServer.ListenAndServe())
+	log.Fatal(server.httpServer.ListenAndServe())
 }
