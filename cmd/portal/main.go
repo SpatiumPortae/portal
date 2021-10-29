308649570e54ab41a5c94f17b9ad0e8ee77b66e3
Zino Kader <zinokad@gmail.com>
2021-10-29 16:58:52 +0200

    read user input for sender app

--- cmd/portal/main.go
+++ cmd/portal/main.go
@@ -1,4 +1,73 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"www.github.com/ZinoKader/portal/models"
+)
+
+const SEND_COMMAND = "send"
+const RECEIVE_COMMAND = "receive"
+
 func main() {
+	if len(os.Args) <= 1 {
+		fmt.Printf("Usage: 'portal %s' to send files and 'portal %s [password]' to receive files\n", SEND_COMMAND, RECEIVE_COMMAND)
+		return
+	}
+
+	sendCmd := flag.NewFlagSet(SEND_COMMAND, flag.ExitOnError)
+	receiveCmd := flag.NewFlagSet(RECEIVE_COMMAND, flag.ExitOnError)
+
+	switch os.Args[1] {
+	case SEND_COMMAND:
+		if len(os.Args) <= 2 {
+			fmt.Println("Provide either one or more files/folder delimited by spaces, or a text string enclosed by quotes.")
+			return
+		}
+		sendCmd.Parse(os.Args[2:])
+		send(sendCmd.Args())
+	case RECEIVE_COMMAND:
+		receiveCmd.Parse(os.Args[2:])
+		receive()
+	default:
+		fmt.Printf("Unrecognized command. Recognized commands: '%s' and '%s'.\n", SEND_COMMAND, RECEIVE_COMMAND)
+	}
+}
+
+func send(fileNames []string) {
+	// compressionCh := make(chan )
+	passCh := make(chan models.Password)
+	files, err := readFiles(fileNames)
+	if err != nil {
+		fmt.Printf("Error reading file(s): %s\n", err.Error())
+		return
+	}
+
+	fileName := "combined"
+	if len(files) == 1 {
+		fileName := files[0].Name()
+	}
+
+}
+
+func receive() {
+
+}
+
+func readFiles(fileNames []string) ([]*os.File, error) {
+	files := make([]*os.File, len(fileNames))
+	for _, fileName := range fileNames {
+		f, err := os.Open(fileName)
+		if err != nil {
+			return nil, fmt.Errorf("file '%s' not found", fileName)
+		}
+		files = append(files, f)
+	}
+	return files, nil
+}
+
+func compressFiles([]*os.File files) {
+	
 }
--- go.mod
+++ go.mod
@@ -4,22 +4,17 @@ go 1.17
 
 require (
 	github.com/gorilla/websocket v1.4.2
+	github.com/schollz/progressbar/v3 v3.8.3
 	github.com/stretchr/testify v1.7.0
 )
 
 require (
-	github.com/charmbracelet/bubbletea v0.18.0 // indirect
-	github.com/containerd/console v1.0.2 // indirect
-	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
-	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
-	github.com/muesli/reflow v0.3.0 // indirect
-	github.com/muesli/termenv v0.9.0 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
-	github.com/schollz/progressbar/v3 v3.8.3 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 	golang.org/x/sys v0.0.0-20211025201205-69cdffdb9359 // indirect
 	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
