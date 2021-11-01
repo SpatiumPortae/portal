69dfe0d45ed7976b9186ae0d3b44fb189539622e
mellonnen <arvidgotthard98@gmail.com>
2021-11-01 11:46:18 +0100

    implemented crypt library with some tests

--- go.mod
+++ go.mod
@@ -18,7 +18,10 @@ require (
 	github.com/nwaples/rardecode v1.1.0 // indirect
 	github.com/pierrec/lz4/v4 v4.0.3 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/schollz/pake/v3 v3.0.2 // indirect
+	github.com/tscholl2/siec v0.0.0-20210707234609-9bdfc483d499 // indirect
 	github.com/ulikunitz/xz v0.5.7 // indirect
 	github.com/xi2/xz v0.0.0-20171230120015-48954b6210f8 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
---
+++ pkg/crypt/crypt_test.go
@@ -0,0 +1,67 @@
+package crypt_test
+
+import (
+	"testing"
+
+	"github.com/schollz/pake/v3"
+	"github.com/stretchr/testify/assert"
+	"www.github.com/ZinoKader/portal/pkg/crypt"
+)
+
+func TestCrypt(t *testing.T) {
+	msg := []byte("A frog walks into a bank...")
+	weakkey := []byte("Normie")
+	t.Run("Encryption", func(t *testing.T) {
+
+		// generate crypt struct.
+		c1, err := crypt.New(weakkey)
+		assert.NoError(t, err)
+
+		// Encrypt decrypt same struct.
+		enc, err := c1.Encrypt(msg)
+		assert.NoError(t, err)
+		dec, err := c1.Decrypt(enc)
+		assert.NoError(t, err)
+		assert.Equal(t, dec, msg)
+
+		// Decrypt using new struct (same salt).
+		c2, err := crypt.New(weakkey, c1.Salt)
+		assert.NoError(t, err)
+		dec, err = c2.Decrypt(enc)
+		assert.NoError(t, err)
+		assert.Equal(t, dec, msg)
+	})
+
+	t.Run("PAKE2 + Encryption", func(t *testing.T) {
+		// Initialize PAKE2 curves.
+		A, err := pake.InitCurve(weakkey, 0, "siec")
+		assert.NoError(t, err)
+		B, err := pake.InitCurve(weakkey, 1, "siec")
+		assert.NoError(t, err)
+
+		// Send A to B.
+		err = B.Update(A.Bytes())
+		assert.NoError(t, err)
+
+		// Send B to A.
+		err = A.Update(B.Bytes())
+		assert.NoError(t, err)
+
+		//Generate sessionkey.
+		kA, err := A.SessionKey()
+		assert.NoError(t, err)
+		kB, err := B.SessionKey()
+		assert.NoError(t, err)
+
+		assert.Equal(t, kA, kB)
+
+		// Encrypt and Decrypt.
+		cA, _ := crypt.New(kA)
+		cB, _ := crypt.New(kB, cA.Salt)
+
+		enc, _ := cA.Encrypt(msg)
+		dec, _ := cB.Decrypt(enc)
+
+		assert.Equal(t, dec, msg)
+	})
+}
