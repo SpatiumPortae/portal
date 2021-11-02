963fb4edd8e048599226b9321af9ca1effb765de
Zino Kader <zinokad@gmail.com>
2021-11-02 23:52:42 +0100

    basic receive func

--- pkg/receiver/receive.go
+++ pkg/receiver/receive.go
@@ -1,16 +1,16 @@
 package receiver
 
 import (
+	"bytes"
 	"encoding/json"
-	"fmt"
-	"log"
 
 	"github.com/gorilla/websocket"
 	"www.github.com/ZinoKader/portal/models/protocol"
 	"www.github.com/ZinoKader/portal/tools"
 )
 
-func (r *Receiver) Receive(wsConn *websocket.Conn) error {
+// TODO: take in expected payload size and progressUpdateCh
+func (r *Receiver) Receive(wsConn *websocket.Conn, expectedPayloadSize int64, progressUpdateCh chan<- float32) (*bytes.Buffer, error) {
 
 	if r.ui != nil {
 		defer close(r.ui)
@@ -19,37 +19,47 @@ func (r *Receiver) Receive(wsConn *websocket.Conn) error {
 	// request payload
 	tools.WriteEncryptedMessage(wsConn, protocol.TransferMessage{Type: protocol.ReceiverRequestPayload}, r.crypt)
 
+	receivedBuffer := &bytes.Buffer{}
 	for {
 		msgType, encBytes, err := wsConn.ReadMessage()
 		if err != nil {
-			return err
+			return nil, err
 		}
 
 		decBytes, err := r.crypt.Decrypt(encBytes)
 		if err != nil {
-			return err
+			return nil, err
 		}
 
 		if msgType == websocket.BinaryMessage {
-			// write to bytes
+			receivedBuffer.Write(decBytes)
+			progressUpdateCh <- float32(receivedBuffer.Len()) / float32(expectedPayloadSize)
 		} else {
 			transferMsg := protocol.TransferMessage{}
 			err = json.Unmarshal(decBytes, &transferMsg)
 			if err != nil {
-				return err
+				return nil, err
 			}
 			if transferMsg.Type != protocol.SenderPayloadSent {
-				return fmt.Errorf("Expected")
+				return nil, protocol.NewWrongMessageTypeError([]protocol.TransferMessageType{protocol.SenderPayloadSent}, transferMsg.Type)
 			}
+			break
 		}
-
 	}
-}
 
-func isExpected(actual protocol.TransferMessageType, expected protocol.TransferMessageType) bool {
-	wasExpected := actual == expected
-	if !wasExpected {
-		log.Printf("Expected message of type: %d. Got type %d\n", expected, actual)
+	// ACK received payload
+	tools.WriteEncryptedMessage(wsConn, protocol.TransferMessage{Type: protocol.ReceiverPayloadAck}, r.crypt)
+
+	transferMsg, err := tools.ReadEncryptedMessage(wsConn, r.crypt)
+	if err != nil {
+		return nil, err
 	}
-	return wasExpected
+	if transferMsg.Type != protocol.SenderClosing {
+		return nil, protocol.NewWrongMessageTypeError([]protocol.TransferMessageType{protocol.SenderClosing}, transferMsg.Type)
+	}
+
+	// ACK SenderClosing with ReceiverClosing
+	tools.WriteEncryptedMessage(wsConn, protocol.TransferMessage{Type: protocol.ReceiverClosingAck}, r.crypt)
+
+	return receivedBuffer, err
 }
