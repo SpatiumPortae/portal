a12382aafad5d932d59591afef996054b54caa3d
mellonnen <arvidgotthard98@gmail.com>
2021-11-02 09:10:25 +0100

    moved password functions to tools

--- tools/password.go
+++ tools/password.go
@@ -1,46 +1,38 @@
-package rendezvous
+package tools
 
 import (
+	"crypto/sha256"
 	"fmt"
 	"math/rand"
-	"sync"
 
 	"www.github.com/ZinoKader/portal/data"
 	"www.github.com/ZinoKader/portal/models"
-	"www.github.com/ZinoKader/portal/tools"
 )
 
 const PasswordWordLength = 3
 
-// generate an untaken password for the target map
-func GeneratePassword(target *sync.Map) models.Password {
+// GeneratePassword generates a random password prefixed with the supplied id.
+func GeneratePassword(id int) models.Password {
 	var words []string
 	hitlistSize := len(data.SpaceWordList)
 
 	// generate three unique words
 	for len(words) != PasswordWordLength {
 		candidateWord := data.SpaceWordList[rand.Intn(hitlistSize)]
-		if !tools.Contains(words, candidateWord) {
+		if !Contains(words, candidateWord) {
 			words = append(words, candidateWord)
 		}
 	}
-
-	var password string
-
-	// find a non-colliding prefix to prepend
-	prefixIndex := 1
-	for {
-		password = formatPassword(prefixIndex, words)
-		_, isTaken := target.Load(password)
-		if !isTaken {
-			break
-		}
-		prefixIndex++
-	}
-
+	password := formatPassword(id, words)
 	return models.Password(password)
 }
 
 func formatPassword(prefixIndex int, words []string) string {
 	return fmt.Sprintf("%d-%s-%s-%s", prefixIndex, words[0], words[1], words[2])
+}
+
+func HashPassword(password models.Password) string {
+	h := sha256.New()
+	h.Write([]byte(password))
+	return string(h.Sum(nil))
 }
