b5e819a9313403610f0daf8cc50d4e59240666cc
mellonnen <arvidgotthard98@gmail.com>
2021-11-05 13:07:59 +0100

    sender state comments

--- pkg/sender/state.go
+++ pkg/sender/state.go
@@ -1,3 +1,4 @@
+// state.go defines the necessary state types and variables for the sender.
 package sender
 
 import "fmt"
@@ -13,16 +14,19 @@ const (
 	WaitForCloseAck
 )
 
+// UIUpdate is a struct that is continously communicated to the UI (if sender has attached a UI)
 type UIUpdate struct {
 	State    TransferState
 	Progress float32
 }
 
+// WrongStateError is a custom error for the Transfer sequence
 type WrongStateError struct {
 	expected TransferState
 	got      TransferState
 }
 
+// WrongStateError constructor
 func NewWrongStateError(expected, got TransferState) *WrongStateError {
 	return &WrongStateError{
 		expected: expected,
@@ -34,6 +38,7 @@ func (e *WrongStateError) Error() string {
 	return fmt.Sprintf("wrong message type, expected type: %d(%s), got: %d(%s)", e.expected, e.expected.Name(), e.got, e.got.Name())
 }
 
+// Name returns the associated to the state enum.
 func (s TransferState) Name() string {
 	switch s {
 	case Initial:
