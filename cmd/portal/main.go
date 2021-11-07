0ab3fb9bd2f80960a2d303be061d29f099ed38cb
Zino Kader <zinokad@gmail.com>
2021-11-08 00:46:49 +0100

    bash, zsh completions

--- cmd/portal/main.go
+++ cmd/portal/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"io"
 	"log"
 	"net"
 	"os"
+	"path"
+	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/jessevdk/go-flags"
@@ -15,11 +18,22 @@ import (
 )
 
 type SendCommandOptions struct{}
-
 type ReceiveCommandOptions struct{}
+type AddCompletionsCommandOptions struct{}
+
+const SHELL_COMPLETION_SCRIPT = `_portal_completions() {
+	args=("${COMP_WORDS[@]:1:$COMP_CWORD}")
+
+	local IFS=$'\n'
+	COMPREPLY=($(GO_FLAGS_COMPLETION=1 ${COMP_WORDS[0]} "${args[@]}"))
+	return 1
+}
+complete -F _portal_completions portal
+`
 
 var sendCommand SendCommandOptions
 var receiveCommand ReceiveCommandOptions
+var addCompletionsCommand AddCompletionsCommandOptions
 
 var programOptions struct {
 	Verbose           string `short:"v" long:"verbose" optional:"true" optional-value:"no-file-specified" description:"Log detailed debug information (optional argument: specify output file with v=mylogfile or --verbose=mylogfile)"`
@@ -29,6 +43,43 @@ var programOptions struct {
 
 var parser = flags.NewParser(&programOptions, flags.Default)
 
+func init() {
+	tools.RandomSeed()
+
+	parser.AddCommand("send",
+		"Send one or more files",
+		"The send command adds one or more files to be sent. Files are archived and compressed before sending.",
+		&sendCommand)
+
+	parser.AddCommand("receive",
+		"Receive files",
+		"The receive command receives files from the sender with the matching password.",
+		&receiveCommand)
+
+	parser.AddCommand("add-completions",
+		"Add command line completions for bash and zsh",
+		"The add-completions command adds command line completions to your shell. Uses the value from the $SHELL environment variable.",
+		&addCompletionsCommand)
+
+	parser.FindOptionByLongName("server").Default = []string{constants.DEFAULT_RENDEZVOUZ_ADDRESS}
+}
+
+// entry point for send/receive commands
+func main() {
+	if _, err := parser.Parse(); err != nil {
+		switch flagsErr := err.(type) {
+		case flags.ErrorType:
+			if flagsErr == flags.ErrHelp {
+				os.Exit(0)
+			}
+			os.Exit(1)
+		default:
+			os.Exit(1)
+		}
+	}
+}
+
+// Execute is executed when the "send" command is invoked
 func (s *SendCommandOptions) Execute(args []string) error {
 	if len(args) == 0 {
 		return errors.New("No files provided. The send command takes file(s) delimited by spaces as arguments.")
@@ -60,6 +111,7 @@ func (s *SendCommandOptions) Execute(args []string) error {
 	return nil
 }
 
+// Execute is executed when the "receive" command is invoked
 func (r *ReceiveCommandOptions) Execute(args []string) error {
 	if len(args) > 1 {
 		return errors.New("Provide a single password, for instance 1-cosmic-ray-quasar.")
@@ -94,35 +146,47 @@ func (r *ReceiveCommandOptions) Execute(args []string) error {
 	return nil
 }
 
-func init() {
-	tools.RandomSeed()
+// Execute is executed when the "add-completions" command is invoked
+func (a *AddCompletionsCommandOptions) Execute(args []string) error {
+	shellBinPath := os.Getenv("SHELL")
+	if len(shellBinPath) == 0 {
+		return fmt.Errorf(
+			"Completions not added - could not find which shell is used.\nTo add completions manually, add the following to your config:\n\n%s", SHELL_COMPLETION_SCRIPT)
+	}
 
-	parser.AddCommand("send",
-		"Send one or more files",
-		"The send command adds one or more files to be sent. Files are archived and compressed before sending.",
-		&sendCommand)
+	shellPathComponents := strings.Split(os.Getenv("SHELL"), "/")
+	usedShell := shellPathComponents[len(shellPathComponents)-1]
+	if !tools.Contains([]string{"bash", "zsh"}, usedShell) {
+		return fmt.Errorf("Unsupported shell \"%s\" at path: \"%s\".", usedShell, shellBinPath)
+	}
 
-	parser.AddCommand("receive",
-		"Receive files",
-		"The receive command receives files from the sender with the matching password.",
-		&receiveCommand)
+	err := writeShellCompletionScript(usedShell)
+	if err != nil {
+		return fmt.Errorf("Failed when adding script to shell config file: %e", err)
+	}
 
-	parser.FindOptionByLongName("server").Default = []string{constants.DEFAULT_RENDEZVOUZ_ADDRESS}
+	fmt.Println("Successfully added completions to your shell config. Run 'source' on your shell config or restart your shell.")
+	return nil
 }
 
-// entry point for send/receive commands
-func main() {
-	if _, err := parser.Parse(); err != nil {
-		switch flagsErr := err.(type) {
-		case flags.ErrorType:
-			if flagsErr == flags.ErrHelp {
-				os.Exit(0)
-			}
-			os.Exit(1)
-		default:
-			os.Exit(1)
-		}
+// writeShellCompletionScript writes the completion script to the specified shell name
+func writeShellCompletionScript(shellName string) error {
+	homedir, err := os.UserHomeDir()
+	if err != nil {
+		return err
 	}
+	shellConfigName := fmt.Sprintf(".%src", shellName)
+	shellConfigPath := path.Join(homedir, shellConfigName)
+	f, err := os.OpenFile(shellConfigPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	if _, err := f.WriteString(fmt.Sprintf("\n# portal shell completion\n%s\n", SHELL_COMPLETION_SCRIPT)); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func validateRendezvousAddress() error {
