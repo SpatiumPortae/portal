c53d803f38dfeb75b48c29d9599f6e345b7104ff
mellonnen <arvidgotthard98@gmail.com>
2022-01-15 23:20:50 +0100

    comments and renaming

--- pkg/receiver/receiver.go
+++ pkg/receiver/receiver.go
@@ -7,16 +7,19 @@ import (
 
 type ReceiverOptions func(*Receiver)
 
+// Receiver struct that encapsulates all necessary information to connect to Rendezvous,
+// exchange cryptographic information and receive files.
 type Receiver struct {
-	crypt             *crypt.Crypt
-	payloadSize       int64
-	rendezvousAddress string
-	rendezvousPort    int
-	ui                chan<- UIUpdate
-	usedRelay         bool
+	crypt             *crypt.Crypt    // Cryptographic information.
+	payloadSize       int64           // Size of payload in bytes.
+	rendezvousAddress string          // Address of the rendezvous server.
+	rendezvousPort    int             // Port that the rendezvous server is running on.
+	ui                chan<- UIUpdate // Channel that can be used to communicate with the UI.
+	usedRelay         bool            // Bool that signifies if the relay is being used for file transfer.
 }
 
-func NewReceiver(programOptions models.ProgramOptions, opts ...ReceiverOptions) *Receiver {
+// New creates a new receiver with the provided options.
+func New(programOptions models.ProgramOptions, opts ...ReceiverOptions) *Receiver {
 
 	r := &Receiver{
 		rendezvousAddress: programOptions.RendezvousAddress,
@@ -28,11 +31,16 @@ func NewReceiver(programOptions models.ProgramOptions, opts ...ReceiverOptions) 
 	return r
 }
 
+// OPTIONS
+
+// WithUI returns an option to set the channel for UI communication.
 func WithUI(ui chan<- UIUpdate) ReceiverOptions {
 	return func(r *Receiver) {
 		r.ui = ui
 	}
 }
+
+// GETTERS
 
 func (r *Receiver) UsedRelay() bool {
 	return r.usedRelay
@@ -49,6 +57,8 @@ func (r *Receiver) RendezvousAddress() string {
 func (r *Receiver) RendezvousPort() int {
 	return r.rendezvousPort
 }
+
+// HELPERS
 
 func (r *Receiver) updateUI(progress float32) {
 	if r.ui == nil {
