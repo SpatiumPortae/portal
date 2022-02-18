0b08655436aec1186819b20f557f1a3243ad28fb
mellonnen <arvidgotthard98@gmail.com>
2022-02-18 13:16:58 +0100

    chore: removed dead code

--- cmd/portal/main.go
+++ cmd/portal/main.go
@@ -14,21 +14,6 @@ import (
 	"www.github.com/ZinoKader/portal/tools"
 )
 
-type SendCommandOptions struct{}
-type ReceiveCommandOptions struct{}
-type AddCompletionsCommandOptions struct{}
-type ServeCommandOptions struct{}
-
-const SHELL_COMPLETION_SCRIPT = `_portal_completions() {
-	args=("${COMP_WORDS[@]:1:$COMP_CWORD}")
-
-	local IFS=$'\n'
-	COMPREPLY=($(GO_FLAGS_COMPLETION=1 ${COMP_WORDS[0]} "${args[@]}"))
-	return 1
-}
-complete -F _portal_completions portal
-`
-
 var (
 	rootCmd = &cobra.Command{
 		Use:   "portal",
@@ -98,96 +83,6 @@ func main() {
 		os.Exit(1)
 	}
 }
-
-// Execute is executed when the "send" command is invoked
-// func (s *SendCommandOptions) Execute(args []string) error {
-// 	if len(args) == 0 {
-// 		return errors.New("No files provided. The send command takes file(s) delimited by spaces as arguments.")
-// 	}
-
-// 	err := validateRendezvousAddress()
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	if len(programOptions.Verbose) != 0 {
-// 		logFileName := programOptions.Verbose
-// 		if programOptions.Verbose == "no-file-specified" {
-// 			logFileName = "portal-send.log"
-// 		}
-// 		f, err := tea.LogToFile(logFileName, "portal-send: ")
-// 		if err != nil {
-// 			return errors.New("Could not log to the provided file.")
-// 		}
-// 		defer f.Close()
-// 	} else {
-// 		log.SetOutput(io.Discard)
-// 	}
-
-// 	handleSendCommand(models.ProgramOptions{
-// 		RendezvousAddress: programOptions.RendezvousAddress,
-// 		RendezvousPort:    programOptions.RendezvousPort,
-// 	}, args)
-// 	return nil
-// }
-
-// // Execute is executed when the "receive" command is invoked
-// func (r *ReceiveCommandOptions) Execute(args []string) error {
-// 	if len(args) > 1 {
-// 		return errors.New("Provide a single password, for instance 1-cosmic-ray-quasar.")
-// 	}
-// 	if len(args) < 1 {
-// 		return errors.New("Provide the password that the file sender gave to you, for instance 1-galaxy-dust-aurora.")
-// 	}
-
-// 	err := validateRendezvousAddress()
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	if len(programOptions.Verbose) != 0 {
-// 		logFileName := programOptions.Verbose
-// 		if programOptions.Verbose == "no-file-specified" {
-// 			logFileName = "portal-receive.log"
-// 		}
-// 		f, err := tea.LogToFile(logFileName, "portal-receive: ")
-// 		if err != nil {
-// 			return errors.New("Could not log to the provided file.")
-// 		}
-// 		defer f.Close()
-// 	} else {
-// 		log.SetOutput(io.Discard)
-// 	}
-
-// 	handleReceiveCommand(models.ProgramOptions{
-// 		RendezvousAddress: programOptions.RendezvousAddress,
-// 		RendezvousPort:    programOptions.RendezvousPort,
-// 	}, args[0])
-// 	return nil
-// }
-
-// // Execute is executed when the "add-completions" command is invoked
-// func (a *AddCompletionsCommandOptions) Execute(args []string) error {
-// 	shellBinPath := os.Getenv("SHELL")
-// 	if len(shellBinPath) == 0 {
-// 		return fmt.Errorf(
-// 			"Completions not added - could not find which shell is used.\nTo add completions manually, add the following to your config:\n\n%s", SHELL_COMPLETION_SCRIPT)
-// 	}
-
-// 	shellPathComponents := strings.Split(os.Getenv("SHELL"), "/")
-// 	usedShell := shellPathComponents[len(shellPathComponents)-1]
-// 	if !tools.Contains([]string{"bash", "zsh"}, usedShell) {
-// 		return fmt.Errorf("Unsupported shell \"%s\" at path: \"%s\".", usedShell, shellBinPath)
-// 	}
-
-// 	err := writeShellCompletionScript(usedShell)
-// 	if err != nil {
-// 		return fmt.Errorf("Failed when adding script to shell config file: %e", err)
-// 	}
-
-// 	fmt.Println("Successfully added completions to your shell config. Run 'source' on your shell config or restart your shell.")
-// 	return nil
-// }
 
 func validateRendezvousAddress() error {
 	rendezvouzAdress := net.ParseIP(viper.GetString("rendezvousAddress"))
