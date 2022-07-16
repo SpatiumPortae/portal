062dfcec62149624a14b3d11bbfe1cf3a5d13343
mellonnen <arvidgotthard98@gmail.com>
2022-07-16 12:53:55 +0200

    feat: implemented E2E test

--- internal/rendezvous/handlers.go
+++ internal/rendezvous/handlers.go
@@ -236,3 +236,11 @@ func startRelay(s *Server, conn conn.Rendezvous, mailbox *Mailbox, mailboxPasswo
 		}
 	}
 }
+
+//nolint:errcheck
+func (s *Server) ping() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("pong"))
+	}
+}
