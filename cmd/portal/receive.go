74f0215599091d48c1d048639c371be05969b7fa
mellonnen <arvidgotthard98@gmail.com>
2023-02-15 00:10:30 +0100

    feat: added password completion to receive command

--- cmd/portal/receive.go
+++ cmd/portal/receive.go
@@ -3,13 +3,17 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 
+	"github.com/SpatiumPortae/portal/data"
 	"github.com/SpatiumPortae/portal/internal/file"
 	"github.com/SpatiumPortae/portal/internal/password"
 	"github.com/SpatiumPortae/portal/internal/semver"
 	"github.com/SpatiumPortae/portal/ui/receiver"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"golang.org/x/exp/slices"
 )
 
 // receiveCmd is the cobra command for `portal receive`
@@ -18,6 +22,30 @@ var receiveCmd = &cobra.Command{
 	Short: "Receive files",
 	Long:  "The receive command receives files from the sender with the matching password.",
 	Args:  cobra.ExactArgs(1),
+	ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+		cobra.CompDebug(toComplete, true)
+		split := strings.Split(toComplete, "-")
+		if len(split) > 4 || len(split) == 0 {
+			return nil, cobra.ShellCompDirectiveNoSpace | cobra.ShellCompDirectiveNoFileComp
+		}
+		if len(split) == 1 {
+			if _, err := strconv.Atoi(split[0]); err != nil {
+				return nil, cobra.ShellCompDirectiveNoSpace | cobra.ShellCompDirectiveNoFileComp
+			}
+			return []string{fmt.Sprintf("%s-", split[0])}, cobra.ShellCompDirectiveNoSpace | cobra.ShellCompDirectiveNoFileComp
+		}
+		suggs := filterPrefix(removeElems(data.SpaceWordList, split[:len(split)-1]), split[len(split)-1])
+		var res []string
+		for _, sugg := range suggs {
+			components := append(split[:len(split)-1], sugg)
+			password := strings.Join(components, "-")
+			if len(components) < 4 {
+				password += "-"
+			}
+			res = append(res, password)
+		}
+		return res, cobra.ShellCompDirectiveNoSpace | cobra.ShellCompDirectiveNoFileComp
+	},
 	PreRun: func(cmd *cobra.Command, args []string) {
 		// Bind flags to viper
 		//nolint
@@ -70,4 +98,53 @@ func handleReceiveCommand(password string) {
 	}
 	fmt.Println("")
 	os.Exit(0)
+}
+
+// ------------------------------------------------ Password Completion ------------------------------------------------
+
+func completionFunc(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+	split := strings.Split(toComplete, "-")
+
+	if len(split) > 4 || len(split) == 0 {
+		return nil, cobra.ShellCompDirectiveNoSpace | cobra.ShellCompDirectiveNoFileComp
+	}
+	if len(split) == 1 {
+		if _, err := strconv.Atoi(split[0]); err != nil {
+			return nil, cobra.ShellCompDirectiveNoSpace | cobra.ShellCompDirectiveNoFileComp
+		}
+		return []string{fmt.Sprintf("%s-", split[0])}, cobra.ShellCompDirectiveNoSpace | cobra.ShellCompDirectiveNoFileComp
+	}
+	// Remove previous components of password, and filter based on prefix
+	suggs := filterPrefix(removeElems(data.SpaceWordList, split[:len(split)-1]), split[len(split)-1])
+	var res []string
+	for _, sugg := range suggs {
+		components := append(split[:len(split)-1], sugg)
+		password := strings.Join(components, "-")
+		if len(components) < 4 {
+			password += "-"
+		}
+		res = append(res, password)
+	}
+	return res, cobra.ShellCompDirectiveNoSpace | cobra.ShellCompDirectiveNoFileComp
+}
+
+func removeElems(src []string, elems []string) []string {
+	var res []string
+	for _, elem := range src {
+		if slices.Contains(elems, elem) {
+			continue
+		}
+		res = append(res, elem)
+	}
+	return res
+}
+
+func filterPrefix(src []string, prefix string) []string {
+	var res []string
+	for _, elem := range src {
+		if strings.HasPrefix(elem, prefix) {
+			res = append(res, elem)
+		}
+	}
+	return res
 }
