4af21dd68f2d6fa47f6fe5ef8506f5ff247c54c8
Zino Kader <zinokad@gmail.com>
2023-02-16 13:22:38 +0100

    format comments

--- internal/rendezvous/handlers.go
+++ internal/rendezvous/handlers.go
@@ -255,8 +255,7 @@ func (s *Server) forwarder(ctx context.Context, wg *sync.WaitGroup, rc conn.Rend
 			return
 		default:
 		}
-		// We give this a timeout in order to be able to detect
-		// context cancellation above.
+		// We give this a timeout in order to be able to detect context cancellation above.
 		timeoutCtx, cancel := context.WithTimeout(ctx, 1*time.Second)
 		payload, err := rc.ReadRaw(timeoutCtx)
 		cancel()
@@ -266,8 +265,7 @@ func (s *Server) forwarder(ctx context.Context, wg *sync.WaitGroup, rc conn.Rend
 		case errors.Is(err, io.EOF):
 			forwardLogger.Error("connection forcefully closed", zap.Error(err))
 			return
-		//TODO: Extract closure status out to the Conn implementation
-		// So we do not tie our logic to tightly to a ws library
+		// TODO: Extract closure status out to the Conn implementation so we do not tie our logic too tightly to a ws library.
 		case websocket.CloseStatus(err) == websocket.StatusNormalClosure:
 			forwardLogger.Info("connection closed, closing forwarder")
 			return
